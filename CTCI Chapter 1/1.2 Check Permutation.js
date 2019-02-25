72dc31fa2146f9931852ebc8320f2611b5f9fc4f
Adrian J Lopez <206582563>
2019-02-25 08:36:48 -0500

    Addition of 1.2 Check Permutation and test for it.

---
+++ CTCI Chapter 1/1.2 Check Permutation.js
@@ -0,0 +1,16 @@
+/**
+* 1.2 Check Permutation: Given two strings, write a method to
+* decide if one is a permutation of the other.
+*/
+exports.default = (firstString, secondString) => {
+  //check if the lengths are the same
+  if (firstString.length !== secondString.length) {
+    return false;
+  } else {
+    //take each string, turn into array, sort it and turn back into string
+    let firstStringSorted = firstString.split('').sort().join('');
+    let secondStringSorted = secondString.split('').sort().join('');
+    //compare both strings
+    return firstStringSorted === secondStringSorted;
+  }
+}
---
+++ test/1.2 Check Permutation.test.js
@@ -0,0 +1,15 @@
+'use strict';
+const chai = require('chai');
+const checkPermutation = require('../CTCI Chapter 1/1.2 Check Permutation').default;
+
+describe('check permutation', () => {
+  it('should fail due to incorrect length', () => {
+    checkPermutation('abc', 'abc ').should.be.false;
+  });
+  it('should fail due to not being a permutation', () => {
+    checkPermutation('abcde', 'abcjk').should.be.false;
+  });
+  it('should pass', () => {
+    checkPermutation('abcde', 'abcde').should.be.true;
+  });
+});
