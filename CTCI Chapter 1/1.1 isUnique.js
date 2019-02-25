2bcb436c64cff70f1a666cc3f28f863d2d4d549b
Adrian J Lopez <206582563>
2019-02-25 08:36:25 -0500

    Updates to comments for 1.1.

--- CTCI Chapter 1/1.1 isUnique.js
+++ CTCI Chapter 1/1.1 isUnique.js
@@ -1,12 +1,17 @@
+'use strict';
 /**
 * 1.1 Is Unique: Implement an algorithm to determine if a string has
 *all unique characters. What if you cannot use additional data structures?
 * O(n^2)
 */
 exports.default = (string) => {
+  //get the length
   const length = string.length;
+  //iterate over the string, character by character,
+  //first loop looks at n, second at n + 1
   for (let i = 0; i < length; i++) {
     for (let j = i + 1; j < length; j++) {
+      //check if current character equals the next one
       if (string[i] === string[j]) {
        return false;
       }
