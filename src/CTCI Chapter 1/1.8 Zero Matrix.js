d4c4a729fec0c3fe8966a24eaa370d1a1ae58c34
Adrian J Lopez <206582563>
2019-02-28 10:35:54 -0500

    Addition of 1.8 Zero Matrix and tests for it.

---
+++ src/CTCI Chapter 1/1.8 Zero Matrix.js
@@ -0,0 +1,44 @@
+'use strict';
+/**
+* 1.8 Zero Matrix: Write an algorithm such that if an element in an
+* MxN matrix is O, its entire row and column are set to 0.
+*/
+
+exports.default = (matrix) => {
+  //check for a valid matrix
+  if (!matrix || !matrix[matrix.length - 1]) {
+    return matrix;
+  }
+  //get the height and width of the matrix
+  const matrixHeight = matrix.length;
+  const matrixWidth = matrix[0].length;
+  //initialize rows and columns to zero out
+  let rows = {};
+  let cols = {};
+  //iterate over the matrix using the height
+  for (let i = 0; i < matrixHeight; i++) {
+    //iterate over the matrix using the width
+    for (let j = 0; j < matrixWidth; ++j) {
+      //if 0 is at this index, then store true
+      if (matrix[i][j] === 0) {
+        rows[i] = true;
+        cols[j] = true;
+      }
+    }
+  }
+  //iterate over items in rows
+  for (let row in rows) {
+    for (let i = 0; i < matrix[row].length; i++) {
+      //zero out this index
+      matrix[row][i] = 0;
+    }
+  }
+  //iterate over items in cols
+  for (let col in cols) {
+    for (let i = 0; i < matrixHeight; i++) {
+      //zero out this index
+      matrix[i][col] = 0;
+    }
+  }
+  return matrix;
+};
---
+++ test/CTCI Chapter 1/1.8 Zero Matrix.test.js
@@ -0,0 +1,30 @@
+'use strict';
+const chai = require('chai');
+const zMatrix = require('../../src/CTCI Chapter 1/1.8 Zero Matrix').default;
+
+describe('Zero Matrix', () => {
+  it('should pass', () => {
+    zMatrix(
+      [
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 0, 1, 1],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1]
+      ]
+    ).should.deep.equal(
+      [
+          [1, 0, 1, 0],
+          [1, 0, 1, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [1, 0, 1, 0],
+          [1, 0, 1, 0]
+      ]
+    );
+  });
+  it('should fail', () => {
+    zMatrix([]).should.deep.equal([]);
+  });
+});
