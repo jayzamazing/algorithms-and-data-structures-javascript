dc5692faef0b4afeecb7723d49f84c49752a8b88
Adrian J Lopez <adrianjaylopez@gmail.com>
2019-03-13 09:58:28 -0400

    Manually dealing with swaps this iteration.

--- src/IC/reverse string array in place.js
+++ src/IC/reverse string array in place.js
@@ -2,9 +2,19 @@
 
 /**
  * Takes an array and returns a reversed array in place
- * 
+ * time: O(n), space: O(1)
  */
 
-let reverseStringArray = (strArray) => strArray.reverse();
+let reverseStringArray = (strArray) => {
+    let leftIndex = 0;
+    let rightIndex = strArray.length - 1;
+    while(leftIndex < rightIndex) {
+        const temp = strArray[leftIndex];
+        strArray[leftIndex] = strArray[rightIndex];
+        strArray[rightIndex] = temp;
+        leftIndex++;
+        rightIndex--;
+    }
+};
 
 module.exports = reverseStringArray;
